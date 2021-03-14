8162f0b6d7724596c899991ea197662426bd0714
detorresramos <detorresramos1@gmail.com>
2021-03-13 21:57:26 -0600

    login page

---
+++ src/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loginPage.css'
+
+function LoginPage() {
+    return (
+        <div class="login-page">
+            loginpage
+        </div>
+    )
+}
+
+export default LoginPage
