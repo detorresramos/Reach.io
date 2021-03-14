3ea1f780b2e5008b6fe25c67dd9f50563113d806
clague <lc61@rice.edu>
2021-03-13 21:20:42 -0600

    trying to add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { FriendCard } from './FriendCard.js';
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router,
+  Redirect
+} from 'react-router-dom'
+import users from './usersJSON.json'
+import Feed from './views/Feed.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        < FriendCard />
+        <Router>
+          <Route path="/" exact component={App}/>
+          <Route path="/feed" component={Feed}/>
+          <Redirect to="/"/>
+          {/* <Route path="/about" component={About}/> */}
+        </Router>
+
+
       </header>
     </div>
   );
