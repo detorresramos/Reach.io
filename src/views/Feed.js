932d97835e9607e45a58df150741ed035fd20abf
clague <lc61@rice.edu>
2021-03-14 03:56:15 -0500

    pushing questionnaire

--- src/views/Feed.js
+++ src/views/Feed.js
@@ -1,7 +1,9 @@
 import { FriendCard } from '../FriendCard.js';
 import Grid from '@material-ui/core/Grid';
+import { Box } from '@material-ui/core';
 import { makeStyles} from '@material-ui/core/styles';
 import BottomNav from '../BottomNav.js';
+import Questionnaire from '../Questionnaire.js';
 
 const users = [
     {
@@ -23,9 +25,15 @@ const users = [
 ]
 
 const useStyles = makeStyles((theme) => ({
-    container: {
+    topBox: {
         // display: "center",
-        paddingTop: "10%",
+        paddingTop: "30px",
+    },
+    bottomBox: {
+        marginBottom: "100px"
+    },
+    spacer: {
+        padding: "10px"
     }
   }));
 
@@ -34,9 +42,9 @@ function Feed() {
     const styles = useStyles();
 
   return (
-    <div className="container" styles={styles.container}>
+    <div className="container" styles={styles.root}>
       <header className="App-header">
-
+        <Box className={styles.topBox}/>
         <Grid 
             container
             direction="column"
@@ -44,10 +52,12 @@ function Feed() {
             alignItems="center"
             styles={styles.container}
         >
+        < Questionnaire />
+        <Box className={styles.spacer}/>
             <div>
                 {users.map(user => {
                     return(
-                        <span>
+                    <span styles={{flex: 1, flexDirection: "row"}}>
                         <Grid container justify={"center"} spacing={5}>
                             <Grid item xs={12} sm={12}>
                                 <FriendCard />
@@ -59,6 +69,7 @@ function Feed() {
             </div>
         </Grid>
       </header>
+      <Box className={styles.bottomBox}/>
       <BottomNav/>
 
     </div>
